d79d8677153cc3cb920c5bd8747224a83cdb7ef8
Sam Yoon <sjaisongyoon@gmail.com>
2019-07-03 16:37:18 -0700

    Heroku Debugging

--- frontend/fundit.jsx
+++ frontend/fundit.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './components/root';
+import Root from './components/Root';
 import * as APIUtil from './util/session_api_util';
 import configureStore from './store/store';
 import {login, logout, signup} from './actions/session_actions'
