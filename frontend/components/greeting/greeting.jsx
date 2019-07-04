8aa2e600c90ac578065fc48f36e17932506a03b4
Sam Yoon <sjaisongyoon@gmail.com>
2019-07-04 09:24:32 -0700

    Changed dropdown attributes when username is clicked

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -5,44 +5,51 @@ import {Link} from 'react-router-dom'
 class Greeting extends React.Component {
     constructor(props){
         super(props);
-        // this.state = {
-        //     clicked: false
-        // }
-        // this.handleClick = this.handleClick.bind(this)
+        this.state = {
+            clicked: false
+        }
+        this.handleClick = this.handleClick.bind(this)
     }
 
-    // handleClick(e){
+    handleClick(e){
 
-    //     e.preventDefault();
-    //     // if (this.state.clicked && e.target.value === "Log Out!"){
-    //     //     this.setState({
-    //     //         clicked: false
-    //     //     });
-    //     //     this.props.logout()
-    //     // } 
-    //     if (this.state.clicked) {
-    //         this.setState({
-    //             clicked: false
-    //         });
-    //     } else {
-    //             this.setState({
-    //             clicked: true
-    //         });
-    //     }
-    // }
+        e.preventDefault();
+        if (this.state.clicked && e.target.value === "Log Out!"){
+            this.setState({
+                clicked: false
+            });
+            this.props.logout()
+        } else if (this.state.clicked) {
+            this.setState({
+                clicked: false
+            });
+        } else if(e.target.className === "trans-overlay"){
+            this.setState({
+                clicked: false
+            })
+        } else {
+                this.setState({
+                clicked: true
+            });
+        }
+    }
 
     render(){
         const {currentUser, logout} = this.props
-        // let className = this.state.clicked ? "dropdown-content-show" : "dropdown-content-hidden";
+        let className = this.state.clicked ? "dropdown-content-show" : "dropdown-content-hidden";
+        let className2 = this.state.clicked ? "trans-overlay" : "dropdown-content-hidden";
         if (currentUser) {
             return (
                 <div className="greeting-message greeting-dropdown">
-                    <input className="greeting-dropdown-input" type="submit" value={currentUser.name}/>
-                    <span className="dropdown-content">
-                        {/* <div> */}
-                            <input className="greeting-dropdown-elements" type="submit" value="Log Out!" onClick={logout} />
-                        {/* </div> */}
-                    </span>
+                    <input className="greeting-dropdown-input" type="submit" value={currentUser.name} onClick={this.handleClick}/>
+                    <div className={className2} onClick={this.handleClick}>
+                        {/* <span className="dropdown-content"> */}
+                        <div className={className}>
+                            <div>
+                                <input className="greeting-dropdown-elements" type="submit" value="Log Out!" onClick={this.handleClick} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             )
         } else {
